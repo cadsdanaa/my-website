2c4acc3fbe63b1ee47473be97655f50c6c02b607
cadsdanaa <cadsdanaa@gmail.com>
2021-09-10 17:11:27 -0500

    Finally switched to AWS Lambda via API Gateway proxy. Updated Go source to handle this as well.

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -9,6 +9,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class MyProjectsComponent implements OnInit {
 
   universe = [] as string[];
+  initialUniverse = {"Universe":
+      "X-----XXX-\n" +
+      "XX-------X\n" +
+      "-XX----XXX\n" +
+      "-XX------X\n" +
+      "-XX------X\n" +
+      "-XXX--X--X\n" +
+      "-XX--XX--X\n" +
+      "-XX------X\n" +
+      "-XX-XXX--X\n" +
+      "---------X"
+  };
 
   constructor(private http: HttpClient) {}
 
@@ -16,13 +28,18 @@ export class MyProjectsComponent implements OnInit {
   }
 
   conwayHttp() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin':'*'
-      })
-    };
-    this.http.get("https://raspberrypi-secure-web-protocol.at.remote.it:30001/conway", httpOptions).subscribe(res => {
+    let ourHeaders = new HttpHeaders()
+      .set('x-api-key', 'l5hvJ8BS937twqRflPlex7Jo4UaViNKB2UEb8dts')
+    let universeToSend;
+    if(this.universe.length == 0) {
+      universeToSend = this.initialUniverse;
+    } else {
+      let temp = this.universe.join("\n");
+      temp = temp.substr(0, temp.length-1);
+      universeToSend = {"Universe": temp};
+    }
+    this.http.post("https://si4cjeooo3.execute-api.us-east-2.amazonaws.com/default/conway", universeToSend, {headers: ourHeaders})
+      .subscribe(res => {
       let resObject = res as any;
       this.universe = resObject['Universe'].split("\n");
     })
